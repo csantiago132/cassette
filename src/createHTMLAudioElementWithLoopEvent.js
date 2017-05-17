98679a35f4c8f367a11f94be20c630c2d7b519e5
Ben Wiley <therealbenwiley@gmail.com>
2017-05-17 11:30:12 -0400

    Custom audio element actually native audio element with MutationObserver on loop.

--- src/createHTMLAudioElementWithLoopEvent.js
+++ src/createHTMLAudioElementWithLoopEvent.js
@@ -1,23 +1,13 @@
 const loopchange = new Event('loopchange');
 
-class HTMLAudioElementWithLoopEvent extends HTMLAudioElement {
-  get loop () {
-    return super.loop;
-  }
-
-  set loop (value) {
-    super.loop = value;
-    this.dispatchEvent(loopchange);
-  }
-}
-
-document.registerElement('audio-with-loop-event', {
-  prototype: HTMLAudioElementWithLoopEvent.prototype,
-  extends: 'audio'
-});
-
 function createHTMLAudioElementWithLoopEvent () {
-  return document.createElement('audio', 'audio-with-loop-event');
+  const audio = document.createElement('audio');
+  new MutationObserver(() => {
+    audio.dispatchEvent(loopchange);
+  }).observe(audio, {
+    attributeFilter: ['loop']
+  });
+  return audio;
 }
 
 module.exports = createHTMLAudioElementWithLoopEvent;

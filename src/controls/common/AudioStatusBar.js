0f51c84f21523f6d589cfcdf226651a157e8898c
Ben Wiley <therealbenwiley@gmail.com>
2017-05-22 02:41:33 -0400

    Audio status info marquee changes style based on element, not window size (using JS).

--- src/controls/common/AudioStatusBar.js
+++ src/controls/common/AudioStatusBar.js
@@ -3,17 +3,66 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import PurePropTypesComponent from './PurePropTypesComponent';
+import getStatusBarSizeClassName from '../../utils/getStatusBarSizeClassName';
 
 class AudioStatusBar extends PurePropTypesComponent {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      elementSizeClassName: null
+    };
+
+    this.statusBarRef = null;
+    this.statusBarResizeObserver = null;
+
+    // bind methods fired on React events
+    this.setStatusBarRef = this.setStatusBarRef.bind(this);
+
+    // bind listeners to add on mount and remove on unmount
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount () {
+    this.statusBarResizeObserver = new ResizeObserver(this.handleResize);
+    this.statusBarResizeObserver.observe(this.statusBarRef);
+  }
+
+  componentWillUnmount() {
+    this.statusBarResizeObserver.disconnect();
+  }
+
+  setStatusBarRef (ref) {
+    this.statusBarRef = ref;
+  }
+
+  handleResize (entries) {
+    for (const entry of entries) {
+      if (entry.target === this.statusBarRef) {
+        const { width } = entry.contentRect;
+        this.setState({
+          elementSizeClassName: getStatusBarSizeClassName(width)
+        });
+        return;
+      }
+    }
+  }
+
   render () {
     const { className, style, displayText, displayTime } = this.props;
     return (
       <div
+        ref={this.setStatusBarRef}
         className={classNames('rr_audio_player__audio_status_bar', className)}
         style={style}
       >
         <div className="rr_audio_player__audio_info_marquee">
-          <div className="rr_audio_player__audio_info">{displayText}</div>
+          <div className={classNames(
+            'rr_audio_player__audio_info',
+            this.state.elementSizeClassName
+          )}>
+            {displayText}
+          </div>
         </div>
         <div className="rr_audio_player__audio_time_progress">{displayTime}</div>
       </div>

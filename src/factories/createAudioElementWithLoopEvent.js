333b8c58e14fdb73eaafec38d8bb54d9ae6843d7
Ben Wiley <therealbenwiley@gmail.com>
2017-06-04 17:16:55 -0400

    Loopchange event not initialized on server side.

--- src/factories/createAudioElementWithLoopEvent.js
+++ src/factories/createAudioElementWithLoopEvent.js
@@ -1,8 +1,13 @@
-const loopchange = new Event('loopchange');
+let loopchange;
 
 function createAudioElementWithLoopEvent () {
   const audio = document.createElement('audio');
   new MutationObserver(() => {
+    // we shouldn't initialize loopchange at the module
+    // level since our code could be run inside of Node
+    // for server rendering (which won't require use of
+    // this module but may still import it).
+    loopchange = loopchange || new Event('loopchange');
     audio.dispatchEvent(loopchange);
   }).observe(audio, {
     attributeFilter: ['loop']

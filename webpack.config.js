0c414f5d7865c17546544e82859de7b4a2add48f
Ben Wiley <therealbenwiley@gmail.com>
2018-03-12 17:41:13 -0400

    Upgrade webpack and include some asset optimizations. Partially addresses #171.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 var webpackConfig = {
-  entry: './src/index.js',
+  entry: {
+    'audioplayer': './src/index.js',
+    'audioplayer.min': './src/index.js'
+  },
   resolve: {
     extensions: ['.js', '.jsx']
   },
@@ -11,7 +15,7 @@ var webpackConfig = {
     publicPath: '/dist',
     libraryTarget: 'umd',
     library: 'AudioPlayer',
-    filename: 'audioplayer.js'
+    filename: '[name].js'
   },
   devServer: {
     inline: true,
@@ -60,9 +64,17 @@ var webpackConfig = {
       amd: 'resize-observer-polyfill'
     }
   },
+  devtool: 'source-map',
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new ExtractTextPlugin('audioplayer.css')
+    new ExtractTextPlugin('[name].css'),
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      sourceMap: true
+    }),
+    new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.min\.css$/
+    })
   ]
 };
 

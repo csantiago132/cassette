3874d79c9e667d5bef668cacc037aeaad5e5b75c
Ben Wiley <therealbenwiley@gmail.com>
2018-03-25 05:28:01 -0400

    getSourceList adjusted to work with sources prop.

--- src/utils/getSourceList.js
+++ src/utils/getSourceList.js
@@ -1,5 +1,9 @@
+import getTrackSources from './getTrackSources';
+
+// collapses playlist into flat list containing
+// the first source url for each track
 function getSourceList (playlist) {
-  return (playlist || []).map(track => track.url);
+  return (playlist || []).map((_, i) => getTrackSources(playlist, i)[0].src);
 }
 
 export default getSourceList;

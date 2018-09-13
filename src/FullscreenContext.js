beb2fa3e1a18fac6531e5e66b402f4113f41ca35
Ben Wiley <therealbenwiley@gmail.com>
2018-09-12 21:22:30 -0400

    Dummy consumer methods filled out for FullscreenContext.

--- src/FullscreenContext.js
+++ src/FullscreenContext.js
@@ -1,8 +1,21 @@
 import createContext from 'create-react-context';
 
+import { logWarning } from './utils/console';
+
 const FullscreenContext = createContext({
   fullscreen: false,
-  requestFullscreen () {}
+  requestFullscreen () {
+    logWarning(
+      'Fullscreen request ignored since there is no ' +
+      'FullscreenContextProvider ancestor.'
+    );
+  },
+  requestExitFullscreen () {
+    logWarning(
+      'Exit fullscreen request ignored since there is no ' +
+      'FullscreenContextProvider ancestor.'
+    );
+  }
 });
 FullscreenContext.Provider.displayName = 'FullscreenContext.Provider';
 FullscreenContext.Consumer.displayName = 'FullscreenContext.Consumer';

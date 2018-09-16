044c59c98bdf83ba62793da053e9d937a89479c8
Ben Wiley <therealbenwiley@gmail.com>
2018-09-16 14:59:20 -0400

    Standard log utility no longer exported (only logWarning and logError).

--- src/utils/console.js
+++ src/utils/console.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 
-export const log = console.log.bind(console);
+const log = console.log.bind(console);
+
 export const logError = console.error ? console.error.bind(console) : log;
 export const logWarning = console.warn ? console.warn.bind(console) : log;

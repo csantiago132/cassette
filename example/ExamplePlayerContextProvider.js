507883d15ac1eb6afb1af91fd72c6c331ea1a659
Ben Wiley <therealbenwiley@gmail.com>
2018-09-16 13:02:59 -0400

    Example updated to use localStorage-synced state snapshots.

--- example/ExamplePlayerContextProvider.js
+++ example/ExamplePlayerContextProvider.js
@@ -1,13 +1,29 @@
-function ExamplePlayerContextProvider(props) {
-  return React.createElement(
-    AudioPlayer.PlayerContextProvider,
-    {
-      playlist: props.playlist,
-      autoplay: true,
-      defaultMuted: true,
-      defaultShuffle: false,
-      crossOrigin: 'anonymous'
-    },
-    props.children
-  );
+class ExamplePlayerContextProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleStateSnapshot = this.handleStateSnapshot.bind(this);
+    this.initialStateSnapshot = JSON.parse(
+      localStorage.getItem('audio_player_snapshot')
+    );
+  }
+
+  handleStateSnapshot(snapshot) {
+    localStorage.setItem('audio_player_snapshot', JSON.stringify(snapshot));
+  }
+
+  render() {
+    return React.createElement(
+      AudioPlayer.PlayerContextProvider,
+      {
+        playlist: this.props.playlist,
+        autoplay: true,
+        defaultMuted: true,
+        defaultShuffle: false,
+        crossOrigin: 'anonymous',
+        initialStateSnapshot: this.initialStateSnapshot,
+        onStateSnapshot: this.handleStateSnapshot
+      },
+      this.props.children
+    );
+  }
 }

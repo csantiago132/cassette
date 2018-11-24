ea320534f6c93284e9b603eaa66abadcfc220611
Ben Wiley <therealbenwiley@gmail.com>
2018-11-24 14:01:03 -0500

    Warn if multiple versions of context loaded.

--- packages/core/src/utils/createSingleGlobalContext.js
+++ packages/core/src/utils/createSingleGlobalContext.js
@@ -1,15 +1,28 @@
 import { createContext } from 'react';
 
+import { logWarning } from './console';
+
+const packageVersion = require('../../package.json').version;
+
 const _global = typeof window === 'undefined' ? global : window;
-_global.__player_context_created__ = _global.__player_context_created__ || {};
+_global.__cassette_contexts__ = _global.__cassette_contexts__ || {};
 
 function createSingleGlobalContext(displayName, defaultValue = null) {
-  if (_global.__player_context_created__[displayName]) {
-    return _global.__player_context_created__[displayName];
+  const ExistingContext = _global.__cassette_contexts__[displayName];
+  if (ExistingContext) {
+    if (ExistingContext.packageVersion !== packageVersion) {
+      logWarning(
+        `Warning: multiple versions of ${displayName} from the @cassette/core` +
+          ` package have been loaded. v${packageVersion} will be ignored and` +
+          ` v${ExistingContext.packageVersion} will be used instead.`
+      );
+    }
+    return ExistingContext;
   }
   const Context = createContext(defaultValue);
   Context.displayName = displayName;
-  _global.__player_context_created__[displayName] = Context;
+  Context.packageVersion = packageVersion;
+  _global.__cassette_contexts__[displayName] = Context;
   return Context;
 }
 
